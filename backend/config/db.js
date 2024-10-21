45e82913469639f95b8b11f114b95d70a3f50c4c
Henri E <henriedwards.sa@gmail.com>
2024-10-21 10:52:44 +0200

    configured database

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user:'root',
+  password:'Justlogin-23',
+  database: 'orb',
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database: ', err);
+  }
+  else {
+    console.log('Connected to mysql database.');
+  }
+});
+
+module.exports = db;
