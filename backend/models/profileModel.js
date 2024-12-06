135e522961492f2a814ff0899587393f6c17abc3
Henri E <henriedwards.sa@gmail.com>
2024-12-06 15:58:36 +0200

    profiles

--- backend/models/profileModel.js
+++ backend/models/profileModel.js
@@ -140,7 +140,9 @@ GROUP BY
       LEFT JOIN accounts ON accounts.profile_id = p.profile_id
       LEFT JOIN invoices ON invoices.profile_id = p.profile_id
       WHERE p.profile_id = ?
-      GROUP BY p.profile_id, ma.name, map.plan_name, pm.first, pm.last, ppm.dependent_nr;
+GROUP BY p.profile_id, ma.name, map.plan_name, pm.first, pm.last, pm.title, pm.id_nr, ppm.dependent_nr;
+
+      
     `;
 
     db.query(query, [profileId, profileId, profileId, profileId], (err, results) => {
