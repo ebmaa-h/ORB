0e8699671d1257b4b06338749c10d2c3d494239d
Henri E <henriedwards.sa@gmail.com>
2025-05-05 14:07:52 +0200

    content navigation update

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -14,59 +14,85 @@ export default function Dashboard() {
     user.features.find((feature) => feature.feature_name === featureName && feature.is_active);
 
   return (
-    <div className='flex flex-col'>        
+    <div className='flex flex-col'>
       <div className='container-col'>
         <p>Hello {user.first}.</p>
-
+        <p>Stats here</p>
+        <p>Hello {user.first}.</p>
+        <p>maybe other details graphs or workflow, not yet sure where ill put the workflow, its quite complicated, might take a full page or if displayed on dash ill have to make it smaller and with a scroll bar</p>
+        <p>Hello {user.first}.</p>
+        <p>Hello {user.first}.</p>
       </div>
+      <div className='flex flex-row gap-4'>
+        <div className='container-col'>
+          <select
+            className="cursor-pointer border rounded border-gray-300 px-2 hover:border-ebmaa-purple transition duration-300 w-[200px] h-[30px]"
+            value={clientId || ""}
+            onChange={(e) => {
+              const selectedClientId = e.target.value;
+              setClientId(selectedClientId);
+              // if (selectedClientId) {
+                //   navigate('/client/info');
+                // }
+              }}
+              >
+            <option disabled={!!clientId}>Select Client</option>
+            {user.client_access.map((client, i) => (
+              <option key={i} className='hover:bg-ebmaa-purple' value={client.client_id}>
+                {client.client_name}
+              </option>
+            ))}
+          </select>
+            <div className={`flex gap-4 ${clientId ? '' : 'disabled'}`}>
 
-      <div className='container-col'>
-        <select
-          className="cursor-pointer border rounded border-gray-300 px-2 hover:border-ebmaa-purple transition duration-300 w-[200px] h-[30px]"
-          value={clientId || ""}
-          onChange={(e) => {
-            const selectedClientId = e.target.value;
-            setClientId(selectedClientId);
-            // if (selectedClientId) {
-            //   navigate('/client/info');
-            // }
-          }}
-        >
-          <option disabled={!!clientId}>Select Client</option>
-          {user.client_access.map((client, i) => (
-            <option key={i} className='hover:bg-ebmaa-purple' value={client.client_id}>
-              {client.client_name}
-            </option>
-          ))}
-        </select>
-          <div className={`flex gap-4 ${clientId ? '' : 'disabled'}`}>
+            {findFeature("profiles") && (
+              <FeatureBlock text="Accounts" link="/accounts" />
+            )}
+            {findFeature("invoices") && (
+              <FeatureBlock text="Invoices" link="/invoices" />
+            )}
+            {findFeature("crq") && (
+              <FeatureBlock text="CRQ" link="/client/info" />
+            )}
+            {findFeature("crq") && (
+              <FeatureBlock text="Reports" link="/client/info" />
+            )}
 
-          {findFeature("profiles") && (
-            <FeatureBlock text="Accounts" link="/accounts" />
-          )}
-          {findFeature("invoices") && (
-            <FeatureBlock text="Invoices" link="/invoices" />
-          )}
-          {findFeature("crq") && (
-            <FeatureBlock text="CRQ" link="/client/info" />
-          )}
-          {findFeature("records") && (
-            <FeatureBlock text="Records" link="/records" />
-          )}
-          {findFeature("records") && (
-            <FeatureBlock text="Profiles" link="/profiles" />
-          )}
+          </div>
         </div>
-      </div>
-      <div className='container-col'>
-        <p>Other</p>
-        <div className={`flex gap-4`}>
-          {findFeature("records") && (
-            <FeatureBlock text="Records" link="/records" />
-          )}
-          {/* {findFeature("records") && (
-            <FeatureBlock text="Profiles" link="/profiles" />
-          )} */}
+        <div className='container-col'>
+          <p>Other</p>
+          <div className={`flex gap-4`}>
+            {findFeature("records") && (
+              <FeatureBlock text="Records" link="/records" />
+            )}
+            {findFeature("records") && (
+              <FeatureBlock text="Profiles" link="/profiles" />
+            )}
+            {/* {findFeature("records") && (
+              <FeatureBlock text="Records" link="/records" />
+              )} */}
+            {/* {findFeature("records") && (
+              <FeatureBlock text="Profiles" link="/profiles" />
+              )} */}
+          </div>
+        </div>
+        <div className='container-col'>
+          <p>Tools</p>
+          <div className={`flex gap-4`}>
+            {findFeature("records") && (
+              <FeatureBlock text="Records" link="/records" />
+            )}
+            {findFeature("records") && (
+              <FeatureBlock text="Profiles" link="/profiles" />
+            )}
+            {/* {findFeature("records") && (
+              <FeatureBlock text="Records" link="/records" />
+              )} */}
+            {/* {findFeature("records") && (
+              <FeatureBlock text="Profiles" link="/profiles" />
+              )} */}
+          </div>
         </div>
       </div>
     </div>
